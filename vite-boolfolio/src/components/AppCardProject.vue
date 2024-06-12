460d09e78c079689b82e541e8e0a8f5aca91b25f
orlando manfredini <orlando.manfre1@gmail.com>
2024-06-12 17:09:14 +0200

    ADD: card component

---
+++ vite-boolfolio/src/components/AppCardProject.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="col-4">
+        <div class="card">
+            <h4>Project Title: {{ project.title }}</h4>
+            <div>{{ project.slug }}</div>
+            <div class="type"> Type: <strong>{{ project.type.type }}</strong></div>
+            <div>
+                Tecnology:
+                <strong class="tecnology" v-for="tecnology in project.tecnologies">
+                {{ tecnology.tecnology }}
+                </strong>
+
+            </div>
+            
+
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['project'],
+        data(){
+            return{
+                tecnology: 0,
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.col-4{
+    width: calc(4 * (100% / 12) - 40px);
+    padding: 20px;
+}
+.card{
+    padding: 20px;
+    border-radius: 6px;
+    border: 1px solid rgb(220, 151, 151);
+}
+.type{
+    margin-top: 7px;
+}
+.tecnology{
+    margin-left: 4px;
+    color:rgb(220, 151, 151);
+}
+</style>
---
+++ vite-boolfolio/src/components/AppMain.vue
@@ -0,0 +1,45 @@
+<template>
+<main>
+    <AppCardProject v-for="(project, i) in projects" :key="i" :project="project" />
+</main>
+</template>
+
+<script>
+import axios from 'axios'
+import AppCardProject from './AppCardProject.vue';
+    export default {
+        components: {
+            AppCardProject,
+        },
+        data(){
+            return{
+               projects: [],
+            };
+            
+        },
+        methods: {
+            fetchProjects(){
+                axios.get('http://127.0.0.1:8000/api/projects')
+                .then((res)=>{
+                    console.log(res.data.projects);
+                    this.projects= res.data.projects.data
+                })
+                
+            }
+        },
+        mounted(){
+            this.fetchProjects();
+        }
+    }
+
+</script>
+
+<style lang="scss" scoped>
+main{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    padding: 40px;
+}
+</style>
